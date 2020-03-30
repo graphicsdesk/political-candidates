0eac06aaa231987a777e9c0a1a8500cf675ca01b
Jason Kao <jason.kao@columbia.edu>
2020-03-30 10:13:54 -0400

    add stylesheet fixes for contributor pages

--- src/script.js
+++ src/script.js
@@ -25,3 +25,16 @@ console.log(`url('data:image/svg+xml;base64,${svg64}')`);
 
 */
 
+// Disable stylesheets on contributor page. Stylesheets are included in the
+// server-side render and styles in files like news-ellipsis.scss
+// still override the site.
+
+if (window.location.pathname.indexOf('/contributors') === 0) {
+  // Disable stylesheets
+  document.querySelectorAll('.story-summary > .twolines > link').forEach(link => link.disabled = true);
+  // Add styles to hide content preview
+  const styleSheet = document.createElement("style");
+  styleSheet.type = "text/css"
+  styleSheet.innerText = '.story-summary > .twolines { display: none; }';
+  document.head.appendChild(styleSheet);
+}
